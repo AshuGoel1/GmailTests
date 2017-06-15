261ad2c4b3d20579a4b70acbbc51b077ca443a2e
Ashu Goel <findashu@gmail.com>
2017-06-15 22:31:56 +0100

    minor cleanup

--- GmailAdvancedSearch.py
+++ GmailAdvancedSearch.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
-import datetime
 from GmailHelper import GmailHelper
 import unittest
 
